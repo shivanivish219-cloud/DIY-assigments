9b702b7d9ba8b5bc0047a290c7fe4dbf597bc039
Shivani Vishwakarma. <shivanivish219@gmail.com>
2026-04-22 00:56:57 +0530

    Add files via upload

---
+++ CB and promise.js
@@ -0,0 +1,31 @@
+function fetchDataWithCallback(callback) {
+  setTimeout(() => {
+    let data = "https://fakestoreapi.com/products/1";
+    callback (data);
+  }, 1000);
+}
+
+setTimeout(() => {
+  callback("from cache");
+}, 2000);
+
+function fetchDataWithpromise() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let data = "https://fakestoreapi.com/products/1";
+      resolve(data);
+    }, 1000);
+
+    setTimeout(() => {
+      resolve("from cache");
+    }, 2000);
+  });
+}
+
+fetchDataWithCallback((data) => {
+  console.log("callback:", data);
+});
+
+fetchDataWithpromise().then((data) => {
+  console.log("promise:", data);
+});
